8f5b9ac8d0d39e63e970499f6aa936c746e9bf20
Ben Weintraub <ben@deckmonitoring.com>
2012-10-23 22:09:31 -0700

    Initial commit of empty Rails 3.2.8 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Laureate::Application.config.secret_token = '8ca7be0ca49980ffbe995a9c472980daca062eeed52ebfcc8c4b96f898c21583c01219c7f0760ec577822d24b2e743b202bcb201c35eb3d1a6b3c4b2758be32e'
